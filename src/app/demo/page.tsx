916342fafc2e178495e7e20011d113cd04428166
“asifulhaque087” <asifulhaque087@gmail.com>
2023-10-15 14:59:24 +0600

    Add format code

--- src/app/demo/page.tsx
+++ src/app/demo/page.tsx
@@ -1,22 +1,22 @@
-'use client'
-import Footer from '@/components/Footer'
-import Header from '@/components/Header'
-import Hero from '@/components/Hero'
-import Card1 from '@/components/cards/Card1'
-import React from 'react'
+"use client";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import Card1 from "@/components/cards/Card1";
+import React from "react";
 
 const page = () => {
   return (
     // <div>this is demo page</div>
     <>
-    <div className='md:mx-[200px] mx-[20px]'>
-    <Header/>
-    <Hero/>
-    <Card1/>
-    <Footer/>
-    </div>
+      <div className="md:mx-[200px] mx-[20px]">
+        <Header />
+        <Hero />
+        <Card1 />
+        <Footer />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default page
+export default page;
---
+++ src/contents/about.ts
@@ -0,0 +1,37 @@
+export const aboutContent = `
+### The International Research Conference Aims and Objectives
+
+The International Research Conference is a federated organization dedicated to bringing together a significant number of diverse scholarly events for presentation within the [conference program](https://waset.org/conferences-in-october-2023-in-lisbon/program "October 2023 in Lisbon Program"). Events will run over a span of time during the conference depending on the number and length of the presentations. With its high quality, it provides an exceptional value for students, academics and industry researchers.
+
+International Conference on Oceanography and Ocean Currents aims to bring together leading academic scientists, researchers and research scholars to exchange and share their experiences and research results on all aspects of Oceanography and Ocean Currents. It also provides a premier interdisciplinary platform for researchers, practitioners and educators to present and discuss the most recent innovations, trends, and concerns as well as practical challenges encountered and solutions adopted in the fields of Oceanography and Ocean Currents.
+
+### Call for Contributions
+
+Prospective authors are kindly encouraged to contribute to and help shape the conference through submissions of their research abstracts, papers and e-posters. Also, high quality research contributions describing original and unpublished results of conceptual, constructive, empirical, experimental, or theoretical work in all areas of Oceanography and Ocean Currents are cordially invited for presentation at the conference. The conference solicits contributions of abstracts, papers and e-posters that address themes and topics of the conference, including figures, tables and references of novel research materials.
+
+### Guidelines for Authors
+
+Please ensure your submission meets the conference's strict guidelines for accepting scholarly papers. Downloadable versions of the check list for [Full-Text Papers](https://waset.org/static/files/help/wasetPaperCheckList.pdf) and [Abstract Papers](https://waset.org/static/files/help/wasetAbstractCheckList.pdf).
+
+Please refer to the [Paper Submission Guideline](https://waset.org/page/paper-submission-guideline), [Abstract Submission Guideline](https://waset.org/page/abstract-submission-guideline) and [Author Information](https://waset.org/page/author-information) before submitting your paper.
+
+### Conference Proceedings
+
+All submitted conference papers will be blind peer reviewed by three competent reviewers. The peer-reviewed conference proceedings are indexed in the [Open Science Index](https://publications.waset.org/), [Google Scholar](https://scholar.google.com/citations?user=kfYkqrMAAAAJ&hl=en), [Semantic Scholar](https://www.semanticscholar.org/search?q=World%20Academy%20of%20Science%2C%20Engineering%20and%20Technology&sort=relevance), [Zenedo](https://zenodo.org/communities/waset/?page=1&size=20), [OpenAIRE](https://www.openaire.eu/search/find/publications?keyword=World%20Academy%20of%20Science,%20Engineering%20and%20Technology), [BASE](https://www.base-search.net/Search/Results?lookfor=World+Academy+of+Science%2C+Engineering+and+Technology+&type=all&oaboost=1&ling=1&name=&newsearch=1&refid=dcbasen), [WorldCAT](https://www.worldcat.org/search?qt=worldcat_org_all&q=World+Academy+of+Science%2C+Engineering+and+Technology), [Sherpa/RoMEO](http://www.sherpa.ac.uk/romeo/search.php?id=566&fIDnum=|&mode=simple&la=en), and other index databases. [Impact Factor Indicators](https://waset.org/page/impact-factor).
+
+### Special Journal Issues
+
+17\. International Conference on Oceanography and Ocean Currents has teamed up with the Special Journal Issue on [Oceanography and Ocean Currents](https://publications.waset.org/marine-and-environmental-sciences). A number of selected high-impact full text papers will also be considered for the special journal issues. All submitted papers will have the opportunity to be considered for this Special Journal Issue. The paper selection will be carried out during the peer review process as well as at the conference presentation stage. Submitted papers must not be under consideration by any other journal or publication. The final decision for paper selection will be made based on peer review reports by the Guest Editors and the Editor-in-Chief jointly. Selected full-text papers will be published online free of charge.
+
+### Conference Sponsor and Exhibitor Opportunities
+
+The Conference offers the opportunity to become a conference sponsor or exhibitor. To participate as a sponsor or exhibitor, please download and complete the [Conference Sponsorship Request Form](https://waset.org/profile/registrations/sponsorship/create/40913/8875).
+
+### Application Forms
+
+- [Paper Submission](https://waset.org/profile/submissions/papers/create/40913/8875)
+- [Author Registration](https://waset.org/profile/registrations/author/create/40913/8875)
+- [Listener Registration](https://waset.org/profile/registrations/listener/create/40913/8875)
+- [Sponsorship and Exhibition](https://waset.org/profile/registrations/sponsorship/create/40913/8875)
+- [Volunteerism](https://waset.org/profile/registrations/volunteerism/create/40913/8875)
+`;
