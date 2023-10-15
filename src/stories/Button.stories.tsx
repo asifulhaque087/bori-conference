2abdd820c99321ecd17681e619f4329e481c6509
“farah-jannat” <farahjannatmithila@gmail.com>
2023-10-15 15:50:02 +0600

    Set up storybook

---
+++ src/stories/Button.stories.tsx
@@ -0,0 +1,71 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Button } from "./Button";
+// import { render } from "react-dom";
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta = {
+    title: "Example/Button",
+    component: Button,
+    parameters: {
+        // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+        layout: "centered",
+    },
+    // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+    tags: ["autodocs"],
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    argTypes: {
+        backgroundColor: { control: "color" },
+    },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+
+export const Primary: Story = {
+    args: {
+        primary: true,
+        label: "Button",
+    },
+};
+
+export const Secondary: Story = {
+    args: {
+        label: "Button",
+    },
+};
+
+export const Large: Story = {
+    args: {
+        size: "large",
+        label: "Button",
+    },
+};
+
+export const Small: Story = {
+    args: {
+        size: "small",
+        label: "Button",
+    },
+};
+
+const Wrapper = () => {
+    return (
+        <div className="p-5 bg-red-500">
+            <h1 className="text-blue-500">hello</h1>
+            <Button label="click-me" />;
+        </div>
+    );
+};
+
+export const Mithila: Story = {
+    parameters: {
+        nextjs: {
+            appDirectory: true,
+        },
+    },
+
+    render: () => <Wrapper />,
+};
